ef0700e0bc526f401c7e7fadb66e5caba231011e
clement-massit <clement.massit@gmail.com>
2021-04-29 10:08:16 +0200

    MoniteurA function display and add created

--- MoniteurA.java
+++ MoniteurA.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class MoniteurA {
+	private Collection<AnomalieEvent> liste_event =  new ArrayList<AnomalieEvent>();
 
+	public MoniteurA() {
+		
+		
+	}
+	private void add_to_list_event(AnomalieEvent event) {
+		liste_event.add(event);
+	}
+	
+	public void display_moniteurA() {
+		System.out.println(liste_event);
+	}
 }

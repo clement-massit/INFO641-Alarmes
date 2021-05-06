5f01165021816c57bd5865628aa48e6810f7c088
Souchon <72502457+romainsouchon@users.noreply.github.com>
2021-05-06 17:54:38 +0200

    Romain est trop fort!
    Interface de la création des events

--- Brouillon.java
+++ Brouillon.java
@@ -8,6 +8,9 @@ import javax.swing.JButton;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 public class Brouillon{
 
@@ -41,7 +44,7 @@ public class Brouillon{
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 480, 270);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
@@ -50,47 +53,68 @@ public class Brouillon{
 		String[] bat = {"Bâtiment A", "Bâtiment B", "Bâtiment C"};
 		panel.setLayout(null);
 		JComboBox comboBox = new JComboBox(bat);
-		comboBox.setBounds(73, 6, 92, 20);
-		panel.add(comboBox);
+		comboBox.setBounds(120, 83, 92, 20);
+		
 		
 		String[] anomalie = {"Gaz", "Incendie", "Radiation"};
 		JComboBox comboBox_1 = new JComboBox(anomalie);
-		comboBox_1.setBounds(175, 6, 87, 20);
+		comboBox_1.setBounds(120, 35, 92, 20);
 		panel.add(comboBox_1);
 		
 		String[] level = {"1", "2", "3"};
 		JComboBox comboBox_2 = new JComboBox(level);
-		comboBox_2.setBounds(272, 6, 55, 20);
-		panel.add(comboBox_2);
+		comboBox_2.setBounds(250, 83, 55, 20);
 		
-		JButton btnAfficher = new JButton("Afficher");
-		btnAfficher.addActionListener(new ActionListener() {
+		JButton btnGene = new JButton("G\u00E9nerer l'\u00E9venement");
+		btnGene.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				String type = comboBox_1.getSelectedItem().toString();
+				String localisation = comboBox.getSelectedItem().toString();
+				int level = Integer.parseInt(comboBox_2.getSelectedItem().toString());
+				
 				Anomalie_Ecouteur ecouteur1 = new Anomalie_Ecouteur();
 				
-				if(comboBox_1.getSelectedItem().toString() == "Gaz") {
-					GazEventSource gaz_source = new GazEventSource();
-					AnomalieEvent anomalie_gaz = new GazEvent(gaz_source);
+				if (type == "Gaz") {
+					GazEventSource gaz_source = new GazEventSource(localisation);
 					gaz_source.addListener(ecouteur1);
-					gaz_source.genEvent();
+					gaz_source.genEvent(level);
 				}
-				if(comboBox_1.getSelectedItem().toString() == "Incendie") {
-					IncendieEventSource incendie_source = new IncendieEventSource();
-					AnomalieEvent anomalie_incendie = new IncendieEvent(incendie_source);
-					incendie_source.addListener(ecouteur1);
-					incendie_source.genEvent();
+				else if (type == "Incendie") {
+					IncendieEventSource inc_source = new IncendieEventSource(localisation);
+					inc_source.addListener(ecouteur1);
+					inc_source.genEvent(level);
 				}
-				if(comboBox_1.getSelectedItem().toString() == "Radiation") {
-					RadiationEventSource radiation_source = new RadiationEventSource();
-					AnomalieEvent anomalie_radiation = new RadiationEvent(radiation_source);
-					radiation_source.addListener(ecouteur1);
-					radiation_source.genEvent();
+				else if (type == "Radiation") {
+					RadiationEventSource rad_source = new RadiationEventSource(localisation);
+					rad_source.addListener(ecouteur1);
+					rad_source.genEvent(level);
 				}
+				
+				
 				
 			}
 		});
-		btnAfficher.setBounds(159, 51, 120, 23);
-		panel.add(btnAfficher);		
+		btnGene.setBounds(147, 135, 172, 23);
+		
+		panel.add(comboBox);
+		panel.add(comboBox_2);
+		panel.add(btnGene);
+		comboBox.setVisible(false);
+		comboBox_2.setVisible(false);
+		btnGene.setVisible(false);
+		
+		JButton BtnType = new JButton("Valider le type");
+		BtnType.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				comboBox.setVisible(true);
+				comboBox_2.setVisible(true);
+				btnGene.setVisible(true);
+			}
+		});
+		BtnType.setBounds(250, 34, 131, 23);
+		panel.add(BtnType);
+		
+		
+		
 	}
-
 }

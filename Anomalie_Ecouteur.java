ad5dd7bc62db9a39b43af5cc7f8ee9d1e95254eb
clement-massit <clement.massit@gmail.com>
2021-05-06 10:44:04 +0200

    Update Anomalie_Ecouteur.java

--- Anomalie_Ecouteur.java
+++ Anomalie_Ecouteur.java
@@ -4,26 +4,26 @@ public class Anomalie_Ecouteur implements GazEventListener, IncendieEventListene
 	@Override
 	public void onEvent(RadiationEvent rad_event) {
 		// TODO Auto-generated method stub
-		/*System.out.println(rad_event);
-		System.out.println("date d'apparition :");
-		rad_event.getDate_apparition();*/
+		System.out.println(rad_event);
+		
+		rad_event.getDate_apparition();
 		
 	}
 
 	@Override
 	public void onEvent(IncendieEvent inc_event) {
 		// TODO Auto-generated method stub
-		/*System.out.println(inc_event);
-		System.out.println("date d'apparition :");
-		inc_event.getDate_apparition();*/
+		System.out.println(inc_event);
+		
+		inc_event.getDate_apparition();
 	}
 
 	@Override
 	public void onEvent(GazEvent gaz_event) {
 		// TODO Auto-generated method stub
-		/*System.out.println(gaz_event);
-		System.out.println("date d'apparition :");
-		gaz_event.getDate_apparition();*/
+		System.out.println(gaz_event);
+		
+		gaz_event.getDate_apparition();
 	}
 
 }

ca3ba707491abbeb524bb7a59b4d618c6b7dd7a6
Souchon <72502457+romainsouchon@users.noreply.github.com>
2021-05-12 16:09:49 +0200

    Création des 6 listes
    1A/2A/3A/1B/2B/3B

--- Liste_anomalie.java
+++ Liste_anomalie.java
@@ -2,49 +2,107 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Liste_anomalie {
-	private Collection<AnomalieEvent> liste_ano_1 =  new ArrayList<AnomalieEvent>();
-	private Collection<AnomalieEvent> liste_ano_2 =  new ArrayList<AnomalieEvent>();
-	private Collection<AnomalieEvent> liste_ano_3 =  new ArrayList<AnomalieEvent>();
+	private Collection<AnomalieEvent> liste_ano_A1 =  new ArrayList<AnomalieEvent>();
+	private Collection<AnomalieEvent> liste_ano_A2 =  new ArrayList<AnomalieEvent>();
+	private Collection<AnomalieEvent> liste_ano_A3 =  new ArrayList<AnomalieEvent>();
+	private Collection<AnomalieEvent> liste_ano_B1 =  new ArrayList<AnomalieEvent>();
+	private Collection<AnomalieEvent> liste_ano_B2 =  new ArrayList<AnomalieEvent>();
+	private Collection<AnomalieEvent> liste_ano_B3 =  new ArrayList<AnomalieEvent>();
 	
-	public Collection<AnomalieEvent> getListe_ano_1() {
-		return liste_ano_1;
-	}
-	public void setListe_ano_1(Collection<AnomalieEvent> liste_ano_1) {
-		this.liste_ano_1 = liste_ano_1;
-	}
-	public Collection<AnomalieEvent> getListe_ano_2() {
-		return liste_ano_2;
-	}
-	public void setListe_ano_2(Collection<AnomalieEvent> liste_ano_2) {
-		this.liste_ano_2 = liste_ano_2;
-	}
-	public Collection<AnomalieEvent> getListe_ano_3() {
-		return liste_ano_3;
-	}
-	public void setListe_ano_3(Collection<AnomalieEvent> liste_ano_3) {
-		this.liste_ano_3 = liste_ano_3;
-	}
 	
 	public void ajout_ano(AnomalieEvent ano) {
 		int level = ano.getNiveau();
+		String type = ano.get_type_anomalie();
 		
 		if(level==1) {
-			liste_ano_1.add(ano);
+			if (type == 'Gaz'){
+				liste_ano_A1.add(ano);
+				liste_ano_B1.add(ano);
+			}
+			else if (type == 'Incendie'){
+				liste_ano_A1.add(ano);	
+			}
+			else if (type == 'Radiation'){
+				liste_ano_B1.add(ano);	
+			}
 		}
 		else if(level==2) {
-			liste_ano_2.add(ano);
+			if (type == 'Gaz'){
+				liste_ano_A2.add(ano);
+				liste_ano_B2.add(ano);
+			}
+			else if (type == 'Incendie'){
+				liste_ano_A2.add(ano);	
+			}
+			else if (type == 'Radiation'){
+				liste_ano_B2.add(ano);	
+			}
 		}
 		else if(level==3) {
-			liste_ano_3.add(ano);
+			if (type == 'Gaz'){
+				liste_ano_A3.add(ano);
+				liste_ano_B3.add(ano);
+			}
+			else if (type == 'Incendie'){
+				liste_ano_A3.add(ano);	
+			}
+			else if (type == 'Radiation'){
+				liste_ano_B3.add(ano);	
+			}
 		}
 	}
+
+
+	public Collection<AnomalieEvent> getListe_ano_A1() {
+		return liste_ano_A1;
+	}
+	public void setListe_ano_A1(Collection<AnomalieEvent> liste_ano_A1) {
+		this.liste_ano_A1 = liste_ano_A1;
+	}
+	public Collection<AnomalieEvent> getListe_ano_A2() {
+		return liste_ano_A2;
+	}
+	public void setListe_ano_A2(Collection<AnomalieEvent> liste_ano_A2) {
+		this.liste_ano_A2 = liste_ano_A2;
+	}
+	public Collection<AnomalieEvent> getListe_ano_A3() {
+		return liste_ano_A3;
+	}
+	public void setListe_ano_A3(Collection<AnomalieEvent> liste_ano_A3) {
+		this.liste_ano_A3 = liste_ano_A3;
+	}
+	public Collection<AnomalieEvent> getListe_ano_B1() {
+		return liste_ano_B1;
+	}
+	public void setListe_ano_B1(Collection<AnomalieEvent> liste_ano_B1) {
+		this.liste_ano_B1 = liste_ano_B1;
+	}
+	public Collection<AnomalieEvent> getListe_ano_B2() {
+		return liste_ano_B2;
+	}
+	public void setListe_ano_B2(Collection<AnomalieEvent> liste_ano_B2) {
+		this.liste_ano_B2 = liste_ano_B2;
+	}
+	public Collection<AnomalieEvent> getListe_ano_B3() {
+		return liste_ano_B3;
+	}
+	public void setListe_ano_B3(Collection<AnomalieEvent> liste_ano_B3) {
+		this.liste_ano_B3 = liste_ano_B3;
+	}
+
+
+	
 	@Override
 	public String toString() {
-		return  "" + liste_ano_1;
-				
-				//*+ "\n liste_ano_2=" + liste_ano_2 + "\n liste_ano_3="
-				//+ liste_ano_3 + "]";*/
+		return "Liste_anomalie [liste_ano_A1=" + liste_ano_A1 + ", liste_ano_A2=" + liste_ano_A2 + ", liste_ano_A3="
+				+ liste_ano_A3 + ", liste_ano_B1=" + liste_ano_B1 + ", liste_ano_B2=" + liste_ano_B2 + ", liste_ano_B3="
+				+ liste_ano_B3 + "]";
 	}
+	
+	
+	
+	
+
 	
 }
 
